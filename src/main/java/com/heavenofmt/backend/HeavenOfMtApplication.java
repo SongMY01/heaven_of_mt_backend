2e7393b1790ed340ade8a181d6cde96a36aaac54
MYONE <1018myone@gmail.com>
2025-10-19 22:09:13 +0900

    Init commit

---
+++ src/main/java/com/heavenofmt/backend/HeavenOfMtApplication.java
@@ -0,0 +1,13 @@
+package com.heavenofmt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavenOfMtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavenOfMtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heavenofmt/backend/HeavenOfMtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heavenofmt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavenOfMtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
